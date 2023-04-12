47d1dde1f48da4713e5fba6bf5bab229a37871cf
Isauro Avila <isauro@avila-ti.com>
2023-04-12 11:14:52 -0600

    project files restructuring

---
+++ lib/data/set_clue.dart
@@ -0,0 +1,24 @@
+import 'package:sudoku/domain/game_control.dart';
+
+import '../domain/sudoku_model.dart';
+import 'error_handler_sudokucell.dart';
+
+/// This function sets the real value of a cell in a Sudoku board if complete certain conditions.
+helpSetSudokuCellValue() {
+  if (gameControl.clues && sudokuBoard.clues > 0) {
+    int x = int.parse(sudokuBoard.selected.split(',')[0]);
+    int y = int.parse(sudokuBoard.selected.split(',')[1]);
+
+    if (x != 9 && y != 9 && !sudokuBoard.cells!['$x,$y']!.bySystem) {
+      sudokuBoard.cells!['$x,$y']!.bySystem = true;
+      sudokuBoard.cells!['$x,$y']!.hadNotes = false;
+      sudokuBoard.cells!['$x,$y']!.value =
+          sudokuBoard.cells!['$x,$y']!.solution;
+      sudokuBoard.clues--;
+      checkAllPad();
+    }
+  }
+  if (sudokuBoard.clues < 1) {
+    gameControl.clues = false;
+  }
+}
